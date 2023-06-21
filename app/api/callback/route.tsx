efb61ab8a30206b16cb5178e9b8516a400a8f94e
Emre <emreozincegedik@gmail.com>
2023-06-21 23:44:28 +0300

    t

--- app/api/callback/route.tsx
+++ app/api/callback/route.tsx
@@ -3,10 +3,7 @@ import crypto from "crypto";
 export async function POST(request: NextRequest) {
   const JOIN_API_KEY = "552ca49562ad4e888a8050b2e829513d";
   const deviceId = "77ddb3e749424e2095f56e47e20ea373";
-  var title = "payment start";
-  var text = "payment start";
-  var url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=${JOIN_API_KEY}&text=${text}&title=${title}&deviceId=${deviceId}`;
-  await fetch(url);
+
   try {
     // ÖNEMLİ UYARILAR!
     // 1) Bu sayfaya oturum (SESSION) ile veri taşıyamazsınız. Çünkü bu sayfa müşterilerin yönlendirildiği bir sayfa değildir.
@@ -16,6 +13,10 @@ export async function POST(request: NextRequest) {
     // siparişin durumunu veri tabanınızdan kontrol edin, eğer onaylandıysa tekrar işlem yapmayın. Örneği aşağıda bulunmaktadır.
 
     var callback = await request.json();
+    var title = "payment start";
+    var text = callback;
+    var url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=${JOIN_API_KEY}&text=${text}&title=${title}&deviceId=${deviceId}`;
+    await fetch(url);
     var merchant_id = "366809";
     var merchant_key = "Erbj6JS1L7514Krx";
     var merchant_salt = "7CYZJMZ8M4c3ig8z";
@@ -50,9 +51,9 @@ export async function POST(request: NextRequest) {
     // }
     return new NextResponse("OK");
   } catch (error: any) {
-    title = "payment error";
-    text = error.toString();
-    url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=${JOIN_API_KEY}&text=${text}&title=${title}&deviceId=${deviceId}`;
-    await fetch(url);
+    // title = "payment error";
+    // text = error.toString();
+    // url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=${JOIN_API_KEY}&text=${text}&title=${title}&deviceId=${deviceId}`;
+    // await fetch(url);
   }
 }
