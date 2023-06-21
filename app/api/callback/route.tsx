de09098fa019edc8a0d3f7bff7402a3bbe6bef32
Emre <emreozincegedik@gmail.com>
2023-06-22 00:09:47 +0300

    ttt

--- app/api/callback/route.tsx
+++ app/api/callback/route.tsx
@@ -12,38 +12,28 @@ export async function POST(request: NextRequest) {
     // veri tabanınızdan ilgili siparişi tespit edip onaylamalı veya iptal etmelisiniz.
     // 3) Aynı sipariş için birden fazla bildirim ulaşabilir (Ağ bağlantı sorunları vb. nedeniyle). Bu nedenle öncelikle
     // siparişin durumunu veri tabanınızdan kontrol edin, eğer onaylandıysa tekrar işlem yapmayın. Örneği aşağıda bulunmaktadır.
-    var title = "payment start";
-    var text = JSON.stringify(request);
-    var url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=${JOIN_API_KEY}&text=${text}&title=${title}&deviceId=${deviceId}`;
-    if (x == 1) {
-      await fetch(url);
-    }
-    x++;
+    return new NextResponse("OK");
+
     var callback = await request.json();
     console.log(callback);
     var merchant_id = "366809";
     var merchant_key = "Erbj6JS1L7514Krx";
     var merchant_salt = "7CYZJMZ8M4c3ig8z";
     // POST değerleri ile hash oluştur.
-    // var paytr_token =
-    //   callback.merchant_oid +
-    //   merchant_salt +
-    //   callback.status +
-    //   callback.total_amount;
+    var paytr_token =
+      callback.merchant_oid +
+      merchant_salt +
+      callback.status +
+      callback.total_amount;
     // console.log(paytr_token);
-    // var token = crypto
-    //   .createHmac("sha256", merchant_key)
-    //   .update(paytr_token)
-    //   .digest("base64");
+    var token = crypto
+      .createHmac("sha256", merchant_key)
+      .update(paytr_token)
+      .digest("base64");
     console.log("here");
     // Oluşturulan hash'i, paytr'dan gelen post içindeki hash ile karşılaştır (isteğin paytr'dan geldiğine ve değişmediğine emin olmak için)
     // Bu işlemi yapmazsanız maddi zarara uğramanız olasıdır.
 
-    // title = "paytr_token: " + paytr_token;
-    // text = "callback: " + JSON.stringify(callback);
-
-    // url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=${JOIN_API_KEY}&text=${text}&title=${title}&deviceId=${deviceId}`;
-    // await fetch(url);
     // if (token != callback.hash) {
     //   throw new Error("PAYTR notification failed: bad hash");
     // }
